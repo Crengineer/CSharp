94f724d330e46277efdde8f629ca0f7e94dbaf24
Marco Crespi <82934270+Crengineer@users.noreply.github.com>
2023-03-09 16:16:43 +0100

    Add files via upload

---
+++ CSharp/CSharpCourse/ConsoleAppArray/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace ConsoleAppArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Monodimensional array
+            Random rnd = new Random(DateTime.Now.Second);
+            int[] array = new int[]
+            {
+            rnd.Next(1024),
+            rnd.Next(1024),
+            rnd.Next(1024)
+            };
+            int[] array2 = new int[2];
+            Console.WriteLine(array[1]);
+
+            // Bidimensional Array
+            string[,] items = new string[,]
+            {
+                {"Frutta", "Pesca" },
+                {"Verdure", "Zucchina" }
+            };
+            Console.WriteLine($"Second array, item {items[0,1]}");
+        }
+    }
+}
